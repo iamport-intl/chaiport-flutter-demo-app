7fabbf9e3cf0e507d58b8c9c70ba3b1c83fe6627
aagam-chai <90909298+aagam-chai@users.noreply.github.com>
2022-03-09 16:55:37 +0530

    - Optimised Code - Displayed Payment Status

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,9 @@
-import 'package:chai_flutter_demo_app/constants/constants.dart';
-import 'package:chai_flutter_demo_app/utils/jwt_token_generation.dart';
-import 'package:chai_flutter_demo_app/utils/signature_hash_generation.dart';
+import 'dart:async';
+import 'package:chai_flutter_demo_app/requests/requests.dart';
+import 'package:chai_flutter_demo_app/result.dart';
 import 'package:chaipay_flutter_package/chaiport_classes/chaiport_impl.dart';
-import 'package:chaipay_flutter_package/dto/requestes/web_checkout_request.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,66 +14,30 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late ChaiPortImpl chai;
-  SignatureHash hash = SignatureHash();
-  JwtTokenGeneration jwt = JwtTokenGeneration();
-  late WebCheckoutRequest orderDetails;
-
-  late String signatureHash;
-  late String jwtToken;
-  late String orderId;
-  late String clientKey;
+  Requests requests = Requests();
+  late StreamSubscription _intentData;
 
   @override
   void initState() {
     super.initState();
-    clientKey = CLIENT_KEY;
-    orderId = getRandomString(6);
-    jwtToken = "Bearer " + jwt.getJWTToken();
-    signatureHash = hash.getSignatureHash("50010", "VND", "https://www.bing.com",
-        orderId, clientKey, "https://www.google.com");
 
+    chai = ChaiPortImpl(context, requests.environment);
+    chai.setPaymentStatusListener(
+        callback: (Map<String, dynamic> paymentStatus) {
+      print('CHAI_PaymentStatus-> $paymentStatus');
+      navigateToResult(paymentStatus);
+    });
+    _intentData = ReceiveSharingIntent.getTextStream().listen((String url) {
+      setState(() {
+        chai.processPaymentStatus(url, requests.environment);
+      });
+    });
+  }
 
-    chai = ChaiPortImpl(context, "sandbox");
-    orderDetails = WebCheckoutRequest(
-        50010,
-        Billing_details(
-            billingAddress: Billing_address(
-                "VND", "VN", "en", "address", "address_2", "400202", "Mah"),
-            billingEmail: "markweins@gmail.com",
-            billingName: "Test mark",
-            billingPhone: "+848959893980"),
-        clientKey,
-        "VN",
-        "VND",
-        false,
-        "By Aagam",
-        "dev",
-        1,
-        "https://www.bing.com",
-        false,
-        Merchant_details(
-            name: "Gumnam",
-            backUrl: null,
-            logo:
-                "https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg",
-            promoCode: null,
-            promoDiscount: 10000,
-            shippingCharges: 10000.00),
-        orderId,
-        <Order_details>[Order_details("knb", "kim nguyen bao", 50010, 1)],
-        "chaipay://checkout",
-        Shipping_details(
-            shippingAddress: Shipping_address(
-                "VND", "VN", "en", "address", "address_2", "400202", "Mah"),
-            shippingEmail: "markweins@gmail.com",
-            shippingName: "Test mark",
-            shippingPhone: "+848959893980"),
-        true,
-        true,
-        signatureHash,
-        "api",
-        "https://www.google.com",
-        "live");
+  @override
+  void dispose() {
+    _intentData.cancel();
+    super.dispose();
   }
 
   @override
@@ -95,11 +57,11 @@ class _HomeState extends State<Home> {
               ),
               ElevatedButton(
                 onPressed: () {
-                  // chai.getOTP("+919913379694");
-                  chai.checkoutUsingWeb(jwtToken, clientKey, orderDetails);
-                  // chai.getPaymentMethods(clientKey);
-                  // chai.getSavedCards(
-                  //     "", clientKey, "+919913379694", "");
+                  // chai.getOTP(requests.mobileNo);
+                  chai.checkoutUsingWeb(requests.getJWTToken(),
+                      requests.clientKey, requests.getRequestBody());
+                  // chai.getPaymentMethods(requests.clientKey);
+                  // chai.getSavedCards("", requests.clientKey, requests.mobileNo, "");
                 },
                 child: const Padding(
                   padding: EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10.0),
@@ -113,9 +75,16 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-  static const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-  final Random _rnd = Random();
 
-  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+  void navigateToResult(Map paymentStatus) {
+    try {
+      Navigator.pop(context);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => Result(paymentStatus: paymentStatus)));
+    } catch (exception) {
+      print(exception.toString());
+    }
+  }
 }
